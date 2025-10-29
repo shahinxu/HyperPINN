512300c20ac8e62fedf3e0242532256d28138f4c
heiroz <1849197801@qq.com>
2025-10-29 17:20:01 -0400

    update

--- HyperPINNTopology.py
+++ HyperPINNTopology.py
@@ -46,26 +46,35 @@ class HyperPINNTopology(nn.Module):
         else:
             raise ValueError("Specify either use_resnet=True or use_attention=True")
         
-        # For pairwise and third-order interactions 
+        # For pairwise up to fifth-order interactions
         num_edges = N * (N-1)//2
         num_triangles = N * (N-1) * (N-2) // 6
-        
-        # Dynamic hypergraph network: input t, output edge and triangle weights
+        # add 4-node and 5-node hyperedges (quadruples and quintuples)
+        num_quads = N * (N-1) * (N-2) * (N-3) // 24
+        num_quints = N * (N-1) * (N-2) * (N-3) * (N-4) // 120
+
+        # Dynamic hypergraph network: input t, output edge/triangle/quad/quint weights
         self.dynamic_hypergraph = nn.Sequential(
             nn.Linear(1, hidden_dim),
             nn.GELU(),
             nn.Linear(hidden_dim, hidden_dim),
             nn.GELU(),
-            nn.Linear(hidden_dim, num_edges + num_triangles)
+            nn.Linear(hidden_dim, num_edges + num_triangles + num_quads + num_quints)
         )
-        
+
         self.lambda_l1_edges = 0.01      
         self.lambda_l1_triangles = 0.01 
+        self.lambda_l1_quads = 0.01
+        self.lambda_l1_quints = 0.01
         self.lambda_l0_edges = 0.001     
         self.lambda_l0_triangles = 0.001 
+        self.lambda_l0_quads = 0.001
+        self.lambda_l0_quints = 0.001
         self.temperature = 1.0           
         self.edge_indices = list(combinations(range(N), 2))
         self.triangle_indices = list(combinations(range(N), 3))
+        self.quad_indices = list(combinations(range(N), 4))
+        self.quint_indices = list(combinations(range(N), 5))
 
     def forward(self, t):
         t = t.float()
@@ -100,38 +109,73 @@ class HyperPINNTopology(nn.Module):
     def get_sparse_weights(self, t, use_concrete=True, hard=False):
         raw_weights = self.dynamic_hypergraph(t)
         num_edges = len(self.edge_indices)
+        num_triangles = len(self.triangle_indices)
+        num_quads = len(self.quad_indices)
+        num_quints = len(self.quint_indices)
+
         edge_logits = raw_weights[:, :num_edges]
-        triangle_logits = raw_weights[:, num_edges:]
-        
+        triangle_logits = raw_weights[:, num_edges:num_edges+num_triangles]
+        quad_logits = raw_weights[:, num_edges+num_triangles:num_edges+num_triangles+num_quads]
+        quint_logits = raw_weights[:, num_edges+num_triangles+num_quads:]
+
         if use_concrete:
             edge_probs = self.concrete_binary_gates(edge_logits, self.temperature, hard)
             triangle_probs = self.concrete_binary_gates(triangle_logits, self.temperature, hard)
+            quad_probs = self.concrete_binary_gates(quad_logits, self.temperature, hard)
+            quint_probs = self.concrete_binary_gates(quint_logits, self.temperature, hard)
         else:
             edge_probs = torch.sigmoid(edge_logits)
             triangle_probs = torch.sigmoid(triangle_logits)
+            quad_probs = torch.sigmoid(quad_logits)
+            quint_probs = torch.sigmoid(quint_logits)
             if hard:
                 edge_probs = (edge_probs > 0.5).float()
-                triangle_probs = (triangle_probs > 0.5).float()  
-        return edge_probs, triangle_probs
+                triangle_probs = (triangle_probs > 0.5).float()
+                quad_probs = (quad_probs > 0.5).float()
+                quint_probs = (quint_probs > 0.5).float()
+
+        return edge_probs, triangle_probs, quad_probs, quint_probs
     
     def sparsity_regularization(self, t):
         raw_weights = self.dynamic_hypergraph(t)
         num_edges = len(self.edge_indices)
+        num_triangles = len(self.triangle_indices)
+        num_quads = len(self.quad_indices)
+        num_quints = len(self.quint_indices)
+
         edge_logits = raw_weights[:, :num_edges]
-        triangle_logits = raw_weights[:, num_edges:]
-        
+        triangle_logits = raw_weights[:, num_edges:num_edges+num_triangles]
+        quad_logits = raw_weights[:, num_edges+num_triangles:num_edges+num_triangles+num_quads]
+        quint_logits = raw_weights[:, num_edges+num_triangles+num_quads:]
+
         edge_probs = torch.sigmoid(edge_logits)
         triangle_probs = torch.sigmoid(triangle_logits)
+        quad_probs = torch.sigmoid(quad_logits)
+        quint_probs = torch.sigmoid(quint_logits)
 
         l1_edges = torch.sum(edge_probs)
         l1_triangles = torch.sum(triangle_probs)
-        l0_edges = torch.sum(edge_probs * (1 - edge_probs) * 4)  
+        l1_quads = torch.sum(quad_probs)
+        l1_quints = torch.sum(quint_probs)
+
+        l0_edges = torch.sum(edge_probs * (1 - edge_probs) * 4)
         l0_triangles = torch.sum(triangle_probs * (1 - triangle_probs) * 4)
-        sparsity_loss = (self.lambda_l1_edges * l1_edges + self.lambda_l1_triangles * l1_triangles +
-                        self.lambda_l0_edges * l0_edges + self.lambda_l0_triangles * l0_triangles)
-        
-        return sparsity_loss, {'l1_edges': l1_edges.item(),'l1_triangles': l1_triangles.item(),
-                               'l0_edges': l0_edges.item(),'l0_triangles': l0_triangles.item()}
+        l0_quads = torch.sum(quad_probs * (1 - quad_probs) * 4)
+        l0_quints = torch.sum(quint_probs * (1 - quint_probs) * 4)
+
+        sparsity_loss = (
+            self.lambda_l1_edges * l1_edges + self.lambda_l1_triangles * l1_triangles +
+            self.lambda_l1_quads * l1_quads + self.lambda_l1_quints * l1_quints +
+            self.lambda_l0_edges * l0_edges + self.lambda_l0_triangles * l0_triangles +
+            self.lambda_l0_quads * l0_quads + self.lambda_l0_quints * l0_quints
+        )
+
+        return sparsity_loss, {
+            'l1_edges': l1_edges.item(), 'l1_triangles': l1_triangles.item(),
+            'l1_quads': l1_quads.item(), 'l1_quints': l1_quints.item(),
+            'l0_edges': l0_edges.item(), 'l0_triangles': l0_triangles.item(),
+            'l0_quads': l0_quads.item(), 'l0_quints': l0_quints.item()
+        }
      
     def physics_loss(self,t):
         x_pred = self.forward(t)
@@ -147,14 +191,22 @@ class HyperPINNTopology(nn.Module):
         k,kD = 0.4,0.3
         coup_rete = torch.zeros_like(xold)
         coup_triangular = torch.zeros_like(xold)
-        
+
         raw_weights = self.dynamic_hypergraph(t)
         num_edges = len(self.edge_indices)
+        num_triangles = len(self.triangle_indices)
+        num_quads = len(self.quad_indices)
+        num_quints = len(self.quint_indices)
+
         edge_weights = raw_weights[:, :num_edges]
-        triangle_weights = raw_weights[:, num_edges:]
-        
+        triangle_weights = raw_weights[:, num_edges:num_edges+num_triangles]
+        quad_weights = raw_weights[:, num_edges+num_triangles:num_edges+num_triangles+num_quads]
+        quint_weights = raw_weights[:, num_edges+num_triangles+num_quads:]
+
         adj_matrix = torch.sigmoid(edge_weights)
         triangle_weights_sigmoid = torch.sigmoid(triangle_weights)
+        quad_weights_sigmoid = torch.sigmoid(quad_weights)
+        quint_weights_sigmoid = torch.sigmoid(quint_weights)
         
         for idx, (i, j) in enumerate(self.edge_indices):
             weight = adj_matrix[:, idx]
@@ -171,8 +223,34 @@ class HyperPINNTopology(nn.Module):
             coup_triangular[:, i] += term_i
             coup_triangular[:, j] += term_j
             coup_triangular[:, k] += term_k
+        # 4-node interactions (quads)
+        coup_quads = torch.zeros_like(xold)
+        for idx, (i, j, k, l) in enumerate(self.quad_indices):
+            weight = quad_weights_sigmoid[:, idx]
+            term_i = weight * (xold[:, j]**2 * xold[:, k] * xold[:, l] - xold[:, i]**4)
+            term_j = weight * (xold[:, i]**2 * xold[:, k] * xold[:, l] - xold[:, j]**4)
+            term_k = weight * (xold[:, i]**2 * xold[:, j] * xold[:, l] - xold[:, k]**4)
+            term_l = weight * (xold[:, i]**2 * xold[:, j] * xold[:, k] - xold[:, l]**4)
+            coup_quads[:, i] += term_i
+            coup_quads[:, j] += term_j
+            coup_quads[:, k] += term_k
+            coup_quads[:, l] += term_l
+        # 5-node interactions (quints)
+        coup_quints = torch.zeros_like(xold)
+        for idx, (i, j, k, l, m) in enumerate(self.quint_indices):
+            weight = quint_weights_sigmoid[:, idx]
+            term_i = weight * (xold[:, j]**2 * xold[:, k] * xold[:, l] * xold[:, m] - xold[:, i]**5)
+            term_j = weight * (xold[:, i]**2 * xold[:, k] * xold[:, l] * xold[:, m] - xold[:, j]**5)
+            term_k = weight * (xold[:, i]**2 * xold[:, j] * xold[:, l] * xold[:, m] - xold[:, k]**5)
+            term_l = weight * (xold[:, i]**2 * xold[:, j] * xold[:, k] * xold[:, m] - xold[:, l]**5)
+            term_m = weight * (xold[:, i]**2 * xold[:, j] * xold[:, k] * xold[:, l] - xold[:, m]**5)
+            coup_quints[:, i] += term_i
+            coup_quints[:, j] += term_j
+            coup_quints[:, k] += term_k
+            coup_quints[:, l] += term_l
+            coup_quints[:, m] += term_m
         
-        term1 = -yold - zold + k * coup_rete + kD * coup_triangular
+        term1 = -yold - zold + k * coup_rete + kD * coup_triangular + kD * coup_quads + kD * coup_quints
         term2 = xold + ar * yold
         term3 = br + zold * (xold - cr)
         term  = torch.cat([term1, term2, term3],dim=1)  
--- Rossler_Oscillators.py
+++ Rossler_Oscillators.py
@@ -10,7 +10,7 @@ from itertools import combinations
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, auc
 
-def roessler_hoi(t, x, EdgeList, TriangleList):
+def roessler_hoi(t, x, EdgeList, TriangleList, QuadList, QuintList):
     m1 = len(x)
     N = m1 // 3
     xold = x[0:N]
@@ -21,6 +21,8 @@ def roessler_hoi(t, x, EdgeList, TriangleList):
 
     coup_rete = np.zeros(N)
     coup_simplicial = np.zeros(N)
+    coup_quads = np.zeros(N)
+    coup_quints = np.zeros(N)
     for ii in range(len(EdgeList)):
         i1 = EdgeList[ii, 0] - 1
         i2 = EdgeList[ii, 1] - 1
@@ -34,7 +36,34 @@ def roessler_hoi(t, x, EdgeList, TriangleList):
         coup_simplicial[i1] += xold[i2]**2 * xold[i3] - xold[i1]**3 + xold[i2] * xold[i3]**2 - xold[i1]**3
         coup_simplicial[i2] += xold[i1]**2 * xold[i3] - xold[i2]**3 + xold[i1] * xold[i3]**2 - xold[i2]**3
         coup_simplicial[i3] += xold[i1]**2 * xold[i2] - xold[i3]**3 + xold[i1] * xold[i2]**2 - xold[i3]**3
-    dxdt1 = -yold - zold + k * coup_rete + kD * coup_simplicial
+    # quads: 4-node hyperedges, follow same style as triangles but higher-order terms
+    if QuadList is not None and len(QuadList) > 0:
+        mquadlist, nquadlist = QuadList.shape
+        for ii in range(mquadlist):
+            i1 = QuadList[ii, 0] - 1
+            i2 = QuadList[ii, 1] - 1
+            i3 = QuadList[ii, 2] - 1
+            i4 = QuadList[ii, 3] - 1
+            coup_quads[i1] += xold[i2]**2 * xold[i3] * xold[i4] - xold[i1]**4
+            coup_quads[i2] += xold[i1]**2 * xold[i3] * xold[i4] - xold[i2]**4
+            coup_quads[i3] += xold[i1]**2 * xold[i2] * xold[i4] - xold[i3]**4
+            coup_quads[i4] += xold[i1]**2 * xold[i2] * xold[i3] - xold[i4]**4
+    # quints: 5-node hyperedges
+    if QuintList is not None and len(QuintList) > 0:
+        mquintlist, nquintlist = QuintList.shape
+        for ii in range(mquintlist):
+            i1 = QuintList[ii, 0] - 1
+            i2 = QuintList[ii, 1] - 1
+            i3 = QuintList[ii, 2] - 1
+            i4 = QuintList[ii, 3] - 1
+            i5 = QuintList[ii, 4] - 1
+            coup_quints[i1] += xold[i2]**2 * xold[i3] * xold[i4] * xold[i5] - xold[i1]**5
+            coup_quints[i2] += xold[i1]**2 * xold[i3] * xold[i4] * xold[i5] - xold[i2]**5
+            coup_quints[i3] += xold[i1]**2 * xold[i2] * xold[i4] * xold[i5] - xold[i3]**5
+            coup_quints[i4] += xold[i1]**2 * xold[i2] * xold[i3] * xold[i5] - xold[i4]**5
+            coup_quints[i5] += xold[i1]**2 * xold[i2] * xold[i3] * xold[i4] - xold[i5]**5
+    dxdt1 = -yold - zold + k * coup_rete + kD * coup_simplicial + kD * coup_quads + kD * coup_quints
+    dxdt1 = -yold - zold + k * coup_rete + kD * coup_simplicial + + kD * coup_quads + kD * coup_quints
     dydt1 = xold + ar * yold
     dzdt1 = br + zold * (xold - cr)
     dxdt = np.concatenate((dxdt1, dydt1, dzdt1))  
@@ -43,10 +72,19 @@ def roessler_hoi(t, x, EdgeList, TriangleList):
 N = 8
 EdgeList = np.array([[1, 2],[2, 3],[3, 4],[5, 6],[6, 7],[7, 8]])
 TriangleList = np.array([[1, 2, 3],[2, 4, 5],[5, 6, 7],[6, 7, 8]])
+# ground-truth 4-node and 5-node hyperedges (follow same style/format)
+QuadList = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+QuintList = np.array([[1, 2, 3, 4, 5], [4, 5, 6, 7, 8]])
+
 all_2edges = list(combinations(range(1, N+1), 2))
 all_3edges = list(combinations(range(1, N+1), 3))
+all_4edges = list(combinations(range(1, N+1), 4))
+all_5edges = list(combinations(range(1, N+1), 5))
+
 true_2edges = set(tuple(sorted(edge)) for edge in EdgeList)
 true_3edges = set(tuple(sorted(triangle)) for triangle in TriangleList)
+true_4edges = set(tuple(sorted(quad)) for quad in QuadList)
+true_5edges = set(tuple(sorted(quint)) for quint in QuintList)
 
 M = 150
 tmax = 20
@@ -54,14 +92,11 @@ dt = tmax / M
 t_eval = np.linspace(0, tmax, M+1)
 t_data = torch.linspace(0, tmax, M+1, requires_grad=True).unsqueeze(1) 
 x0 = np.random.uniform(-1, 1, size=(3 * N,))
-sol = solve_ivp(roessler_hoi, (0,tmax), x0, t_eval=t_eval, args=(EdgeList, TriangleList))
+sol = solve_ivp(roessler_hoi, (0,tmax), x0, t_eval=t_eval, args=(EdgeList, TriangleList, QuadList, QuintList))
 X = sol.y.T 
 nt = len(t_eval)
-dxdt = np.array([roessler_hoi(t, sol.y[:, i], EdgeList, TriangleList) for i, t in enumerate(sol.t)])
+dxdt = np.array([roessler_hoi(t, sol.y[:, i], EdgeList, TriangleList, QuadList, QuintList) for i, t in enumerate(sol.t)])
 
-## Add noise
-#noise_level = 0.1 * np.std(sol.y) 
-#X += noise_level * np.random.randn(*X.shape)
 x_data = torch.tensor(X, dtype=torch.float64) 
 
 architectures = [("ResNet", True, False),("Attention", False, True)]    
@@ -92,17 +127,23 @@ def get_labels_and_scores(all_edges, true_edges, probs):
         y_score.append(probs[idx])
     return np.array(y_true), np.array(y_score)
 
-def evaluate_edges_triangles(model, t_data, all_2edges, true_2edges, all_3edges, true_3edges):
+def evaluate_edges_triangles(model, t_data, all_2edges, true_2edges, all_3edges, true_3edges, all_4edges, true_4edges, all_5edges, true_5edges):
     with torch.no_grad():
-        edge_probs, triangle_probs = model.get_sparse_weights(t_data, use_concrete=False, hard=False)
+        edge_probs, triangle_probs, quad_probs, quint_probs = model.get_sparse_weights(t_data, use_concrete=False, hard=False)
         # Take the last time step for evaluation
         edge_probs = edge_probs[-1].cpu().numpy()
         triangle_probs = triangle_probs[-1].cpu().numpy()
+        quad_probs = quad_probs[-1].cpu().numpy()
+        quint_probs = quint_probs[-1].cpu().numpy()
     edge_scores = [abs(edge_probs[idx]) for idx, _ in enumerate(all_2edges)]
     triangle_scores = [abs(triangle_probs[idx]) for idx, _ in enumerate(all_3edges)]
+    quad_scores = [abs(quad_probs[idx]) for idx, _ in enumerate(all_4edges)]
+    quint_scores = [abs(quint_probs[idx]) for idx, _ in enumerate(all_5edges)]
     y_true_2, y_score_2 = get_labels_and_scores(all_2edges, true_2edges, edge_scores)
     y_true_3, y_score_3 = get_labels_and_scores(all_3edges, true_3edges, triangle_scores)
-    return y_true_2, y_score_2, y_true_3, y_score_3
+    y_true_4, y_score_4 = get_labels_and_scores(all_4edges, true_4edges, quad_scores)
+    y_true_5, y_score_5 = get_labels_and_scores(all_5edges, true_5edges, quint_scores)
+    return y_true_2, y_score_2, y_true_3, y_score_3, y_true_4, y_score_4, y_true_5, y_score_5
 
 def compute_auc(y_true, y_score):
     fpr, tpr, _ = roc_curve(y_true, y_score)
@@ -157,17 +198,23 @@ for epoch in range(epochs):
          print(f"  Physics: {physics_loss.item():.6f}, Data: {data_loss.item():.6f}")
          print(f"  Sparsity: {sparsity_loss.item():.6f}")
          print(f"  L1 edges: {sparsity_info['l1_edges']:.2f}, L1 triangles: {sparsity_info['l1_triangles']:.2f}")
-         y_true_2, y_score_2, y_true_3, y_score_3 = evaluate_edges_triangles(model, t_data, all_2edges, true_2edges, all_3edges, true_3edges)
+         y_true_2, y_score_2, y_true_3, y_score_3, y_true_4, y_score_4, y_true_5, y_score_5 = \
+             evaluate_edges_triangles(model, t_data, all_2edges, true_2edges, all_3edges, true_3edges, all_4edges, true_4edges, all_5edges, true_5edges)
          auc_2 = compute_auc(y_true_2, y_score_2)
          auc_3 = compute_auc(y_true_3, y_score_3)
-         print(f"  AUC (2-edges): {auc_2:.4f}, AUC (3-edges): {auc_3:.4f}")
+         auc_4 = compute_auc(y_true_4, y_score_4)
+         auc_5 = compute_auc(y_true_5, y_score_5)
+         print(f"  AUC (2-edges): {auc_2:.4f}, AUC (3-edges): {auc_3:.4f}, AUC (4-edges): {auc_4:.4f}, AUC (5-edges): {auc_5:.4f}")
 
-y_true_2, y_score_2, y_true_3, y_score_3 = evaluate_edges_triangles(model, t_data, all_2edges, true_2edges, all_3edges, true_3edges)
-y_true_total = np.concatenate([y_true_2, y_true_3])
-y_score_total = np.concatenate([y_score_2, y_score_3])   
+y_true_2, y_score_2, y_true_3, y_score_3, y_true_4, y_score_4, y_true_5, y_score_5 = \
+    evaluate_edges_triangles(model, t_data, all_2edges, true_2edges, all_3edges, true_3edges, all_4edges, true_4edges, all_5edges, true_5edges)
+y_true_total = np.concatenate([y_true_2, y_true_3, y_true_4, y_true_5])
+y_score_total = np.concatenate([y_score_2, y_score_3, y_score_4, y_score_5])   
 plt.figure(figsize=(8, 6))
 plot_roc(y_true_2, y_score_2, 'Pairwise')
 plot_roc(y_true_3, y_score_3, 'Third-order')
+plot_roc(y_true_4, y_score_4, 'Fourth-order')
+plot_roc(y_true_5, y_score_5, 'Fifth-order')
 plot_roc(y_true_total, y_score_total, label='All') 
 plt.plot([0, 1], [0, 1], 'k--', label='Random Guess')
 plt.xlabel('False Positive Rate',fontsize=16)
